4d92d31364199b5063ec7c7293c9692e30ced4db
Harshit Joshi <hpj1992@gmail.com>
2016-03-13 21:26:56 -0700

    Added Directory Added 2 more programs.

---
+++ .idea/src/Medium/LongestPalindromicSubstring.java
@@ -0,0 +1,39 @@
+/*
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
+
+public class LongestPalindromicSubstring {
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String question = "abcba";
+        if(question.length()==1)
+            System.out.println("Length:1");
+        String max = "";
+        for(int i=0;i<question.length();i++)
+        {
+            String s1 = findPalindromeLengthFromThisIndex(i,i, question.length(),question.toCharArray());
+            max = s1.length()>max.length()?s1:max;
+            String s2 = findPalindromeLengthFromThisIndex(i,i+1, question.length(),question.toCharArray());
+            max = s2.length()>max.length()?s2:max;
+
+        }
+        System.out.println("Longest:"+max);
+
+    }
+
+    public static String findPalindromeLengthFromThisIndex(int left,int right,int length,char[] problem)
+    {
+
+        while(left >=0 && right < length && problem[left] == problem[right] )
+        {
+            left--;
+            right++;
+        }
+
+        String palindrom = String.valueOf(problem).substring(left+1, right);
+        return palindrom;
+
+    }
+}
---
+++ .idea/src/Medium/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,43 @@
+/*
+Given a string, find the length of the longest substring without repeating characters.
+For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3.
+For "bbbbb" the longest substring is "b", with the length of 1.
+ */
+class LongestSubstringNoRepeats_3 {
+    public static void main(String[] args) {
+        System.out
+                .println("Length of Longest substring without repeating characters: "
+                        + lengthOfLongestSubstring("abcdefghiapoewrd "));
+        System.out.println(lengthOfLongestSubstring2("abcdefghiapoewrd "));
+    }
+
+
+    public static int lengthOfLongestSubstring2(String s)
+    {
+        if(s==null || s.length() == 0)
+            return 0;
+        else
+        {
+            int maxLength = 0;
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<s.length();i++)
+            {
+                char character = s.charAt(i);
+                if(sb.indexOf(String.valueOf(character)) >= 0)
+                {
+                    maxLength = (maxLength > sb.length()) ? maxLength : sb.length();
+                    sb = new StringBuilder(sb.substring(sb.indexOf(String.valueOf(character))+1));
+                    sb.append(character);
+                }
+                else
+                {
+                    sb.append(String.valueOf(character));
+                }
+            }
+            maxLength = (maxLength > sb.length()) ? maxLength : sb.length();
+            return maxLength;
+        }
+
+    }
+}
+
