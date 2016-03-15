427b56e7a6abc49dc338ac87f7cbefd29972a94e
Harshit Joshi <hpj1992@gmail.com>
2016-03-14 21:57:13 -0700

    Integer to roman.

---
+++ .idea/src/Medium/IntegerToRoman.java
@@ -0,0 +1,29 @@
+
+/*
+Given an integer, convert it to a roman numeral.
+
+Input is guaranteed to be within the range from 1 to 3999.
+ */
+public class IntegerToRoman
+{
+    public String intToRoman(int num) {
+        if(num == 0)
+            return "";
+        int[] value = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String data[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder ans = new StringBuilder();
+        for(int i=0;i<value.length;i++)
+        {
+            int devisor = value[i];
+            int base = num/devisor;
+            if(base != 0)
+            {
+                while(base--!=0)ans.append(data[i]);
+                num = num % devisor;
+            }
+        }
+        return ans.toString();
+
+
+    }
+}
