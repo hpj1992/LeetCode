8d2d08c66d7e0f94710e3b5d90cee6c76d86e37e
Harshit Joshi <hpj1992@gmail.com>
2016-03-17 18:14:12 -0700

    Letter combination of phone numbers

---
+++ .idea/src/Medium/LetterCombinationOfPhoneNumber.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCombinationOfPhoneNumber
+{
+    public static void main(String[] args)
+    {
+        List<String> allWords = letterCombinations("23");
+        for(int i=0; i< allWords.size(); i++)
+        {
+            System.out.println(allWords.get(i));
+        }
+    }
+    public static List<String> letterCombinations(String digits) {
+
+        if(digits == null || digits.length() == 0)
+            return new ArrayList<String>();
+        String[] setup = { "0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+        StringBuilder sb = new StringBuilder();
+        ArrayList<String> ans = new ArrayList<String>();
+        ans.add("");
+        for( int i = 0; i < digits.length() ; i++)
+        {
+            int letter = Character.getNumericValue(digits.charAt(i));
+            String letters = setup[letter];
+            int curLength = ans.size();
+            while(curLength > 0)
+            {
+                String value = ans.get(curLength-1);
+                for(int j=0;j<letters.length();j++)
+                {
+                    ans.add(value+letters.charAt(j));
+                }
+                ans.remove(curLength-1);
+                curLength--;
+            }
+        }
+        return ans;
+    }
+}
