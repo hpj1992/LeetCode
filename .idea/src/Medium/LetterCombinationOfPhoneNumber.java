799393a5cabe59001dffa0e9a78f19471baee231
Harshit Joshi <hpj1992@gmail.com>
2016-03-18 17:12:05 -0700

    A

--- .idea/src/Medium/LetterCombinationOfPhoneNumber.java
+++ .idea/src/Medium/LetterCombinationOfPhoneNumber.java
@@ -1,3 +1,14 @@
+/*
+Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
